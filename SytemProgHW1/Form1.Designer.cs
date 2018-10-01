7d09d5a715cd019d3db018bc08709407e17b915f
valikneskazhu94 <valik_ne_skazhu@ukr.net>
2018-10-01 20:42:42 +0300

    SystemHW1

---
+++ SytemProgHW1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SytemProgHW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processesAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // processesAll
+            // 
+            this.processesAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.processesAll.Location = new System.Drawing.Point(711, 64);
+            this.processesAll.Name = "processesAll";
+            this.processesAll.Size = new System.Drawing.Size(170, 49);
+            this.processesAll.TabIndex = 0;
+            this.processesAll.Text = "Показать все процессы.";
+            this.processesAll.UseVisualStyleBackColor = true;
+            this.processesAll.Click += new System.EventHandler(this.processesAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(653, 428);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Запущенные процессы в системе:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 518);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.processesAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button processesAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SytemProgHW1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SytemProgHW1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void processesAll_Click(object sender, EventArgs e)
+        {
+            foreach (Process item in Process.GetProcesses())
+            {
+                ListViewItem lvi = new ListViewItem();
+                //выводим на экран Id и Name запущенных процессов
+                //lvi.Add(item.Id.ToString()+ "; "+item.ProcessName.ToString());
+                lvi.Text = "ID процесса: "+item.Id.ToString()+"; Имя процесса: "+item.ProcessName.ToString();
+                listView1.Items.Add(lvi);
+            }
+           
+        }
+    }
+}
